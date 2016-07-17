ad746b540251a9f54e41b2d9655b85b78a787ffe
jensbodal <jensbodal@gmail.com>
2016-07-17 16:36:53 -0700

    modified api endpoint for error handling of bad requests

--- src/client/app/home/home.controller.js
+++ src/client/app/home/home.controller.js
@@ -6,12 +6,18 @@
     .controller('HomeController', HomeController);
   
   HomeController.$inject = [
+    '$localStorage'
   ];
 
-  function HomeController() {
+  function HomeController($localStorage) {
     var vm = this;
     vm.pageTitle = 'Dynamic Page Title';
     vm.checkbox1 = true;
+    vm.getUsername = getUsername;
+
+    function getUsername() {
+      return $localStorage.authenticatedUser;
+    }
   }
 
 })();
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -6,133 +6,195 @@ var ObjectId = require('mongodb').ObjectId;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var response = {
-    'HTTP OK': '200'
+    'HTTP OK': '200',
+    'doneyet': 'welcome'
   };
-  res.send(response);
-});
-
-/* GET a specific timer by oid */
-router.get('/timer', function(req, res, next) {
-  console.log('[/timer/what?] ' + req.headers.authorization);
-  var oid = new ObjectId(req.query.oid);
-  app.dbo.collection('timers').findOne(
-    {'_id':oid}, 
-    function(err, result) {
-      if (err) {
-        console.log('ERROR');
-        res.send({'ERROR':'ERROR MESSAGE'});
-      }
-      else {
-        res.send(result);
-      }
-  });
+  res.status(200).send(response);
 });
 
 /* GET a specific timer by _id */
 router.get('/timers/:oid', function(req, res, next) {
-  console.log('[/timers/:oid] ' + req.headers.authorization);
-  var oid = new ObjectId(req.params.oid);
-  app.dbo.collection('timers').findOne(
-    {'_id':oid}, 
-    function(err, result) {
-      if (err) {
-        console.log('ERROR');
-        res.send({'ERROR':'ERROR MESSAGE'});
+  try {
+    var uuid = new ObjectId(req.headers.uuid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+
+  try {
+    var oid = new ObjectId(req.params.oid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid Timer ID Format', error: error.toString()});
+  }
+ 
+  app.dbo.collection('timers').findOne({'_id':oid})
+  .then(function(results) {
+    if (results == null) {
+      res.status(404).send({message: 'Timer not found'});
+    }
+    else {
+      var stringIds = results.userIds.map(function(userId) {
+        return userId = userId.toString();
+      });
+      if (stringIds.indexOf(uuid.toString()) != -1) {
+        res.send(results);
       }
       else {
-        res.send(result);
+        res.status(403).send({message: 'Specified user does not have permission to view the requested timer'});
       }
-  });
+    }
+  });    
 });
 
 /* GET all timers */
 router.get('/timers', function(req, res, next) {
-  console.log('[/timers] GET');
+  try {
+    var uuid = new ObjectId(req.headers.uuid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+ 
   var id = req.params.id;
-  var something = app.dbo.collection('timers').find()
-    .toArray(function(err, result) {
-      if (err) {
-        console.log('ERROR');
-        res.send({'ERROR':'ERROR MESSAGE'});
-      }
-      else {
-        res.send(result);
-      }
+  
+  app.dbo.collection('users').findOne({_id: uuid})
+  .then(function foundUser(user) {
+    if (user == null) {
+      res.status(404).send({message: 'User not found'});
+    }
+    else {
+      app.dbo.collection('timers').find().toArray()
+      .then(function foundTimers(results) {
+        // if uuid is in the timers list of userIds then include it
+        var timers = results.filter(result => 
+          result.userIds.filter(id => id.toString() === uuid.toString()).length > 0);
+
+        res.send(timers);
+      });
+    }
   });
 });
 
 /* POST add new timer */
 router.post('/timers', function(req, res) {
-  console.log('[/timers] POST ');
-  var token = req.headers.token;
-  var username = req.headers.username;
-  var uuid = req.headers.uuid;
+  try {
+    var uuid = new ObjectId(req.headers.uuid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+  
   var timer = req.body;
   
   timer.userIds = [
-    new ObjectId(uuid)
+    uuid
   ];
-  if (validateTimer(timer)) {
-    app.dbo.collection('timers').insertOne(
-      timer, 
-      function(err, result) {
-        if (err) {
-          console.log('ERROR');
-          res.status(500).send({ error:'Unabled to create new timer'});
-        }
-        else {
-          res.send(result);
-        }
+  
+  if (validateTimer(timer) && !timer._id) {
+    app.dbo.collection('users').findOne({_id: uuid})
+    .then(function(result) {
+      if (result === null) {
+        res.status(404).send({message: 'User ID not found'});
+      }
+      else {
+        app.dbo.collection('timers').insertOne(timer)
+        .then(function(result) {
+          app.dbo.collection('users')
+          .updateOne({_id: new ObjectId(uuid)},
+            {$push: {timers: new ObjectId(timer._id)}})
+            .then(res.send({ message: 'new timer created successfully', timerId: timer._id}))
+        })
+      } // end else
+    }, function(error) {
+      console.log("ERROR ADDING TIMER");
+      res.status(500).send({ message: 'Unable to create new timer', error: error});
     });
   }
   else {
-    res.status(400).send({ error: 'Invalid Timer Object', 'data':timer });    
+    var error = "Invalid timer object";
+    if (timer._id) {
+      error += '. You cannot specify your own ID';
+    }
+    res.status(400).send({ error: error, 'data':timer });    
   }
 });
 
 /* DELETE an existing timer */
 router.delete('/timers/:oid', function(req, res) {
-  console.log('[/timers/:oid] DEL ' + req.headers.authorization);
-  var oid = new ObjectId(req.params.oid);
+  try {
+    var uuid = new ObjectId(req.headers.uuid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+
+  try {
+    var oid = new ObjectId(req.params.oid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid Timer ID Format', error: error.toString()});
+  }
+
   app.dbo.collection('timers')
   .removeOne({'_id': oid})
-  .then(function(succ) {
-    res.send('OK');
+  .then(function(results) {
+    if (results === null || results.deletedCount === 0) {
+      res.status(404).send({message: 'Timer ID could not be found'});
+    }
+    else {
+      console.log(results);
+      res.send({message: 'timer has been deleted', timerId: oid});
+    }
   }, function error(result) {
     console.log('error');
-    res.send('FAIL');
+    res.status(500).send({message: 'error deleting timer', error: result});
   });
 });
 
 /* PUT an existing timer */
 router.put('/timers', function(req, res) {
-  console.log('[/timers] PUT ' + req.headers.authorization);
+  try {
+    var uuid = new ObjectId(req.headers.uuid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+
   var timer = req.body;
-  var oid = new ObjectId(timer._id);
-  delete timer._id; // cannot reinsert same ID so need to remove this from our insert object
+  try {
+    var oid = new ObjectId(timer._id);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid Timer ID Format', error: error.toString()});
+  }
+
+  // cannot reinsert same ID so need to remove this from our insert object
+  // no database calls needed as we are just preparing the object for updating
+  delete timer._id; 
 
   if (validateTimer(timer)) {
     console.log('Update action requested');
     console.log(oid);
     console.log(timer.name);
     app.dbo.collection('timers')
-    .findOne({_id: oid}, function (err, response) {
-      if (false) {
-        console.log("???????????????????????");
-        res.status(500).send("ERROR UPDATING TIMER");
+    .findOne({_id: oid})
+    .then(function found(result) {
+      if (result === null) {
+        res.status(404).send({message: 'Timer ID not found'});
       }
       else {
-        console.log("FOUND IT");
-        timer.userIds = response.userIds;
+        timer.userIds = result.userIds;
         app.dbo.collection('timers')
-        .updateOne({'_id':oid}, timer)
+        .updateOne({'_id': oid}, timer)
         .then(function(succ) {
-          res.send('OK');
-        }, function error(result) {
-          console.log(result);
-          console.log('error updating object');
-          res.status(500).send({ error: 'Server could not find object to update'});
-      })
+          res.status(200).send({message: 'Timer updated', timerId: oid});
+        });
+      }
+    }, function error(result) {
+      console.log('DIDNT FIND or ERROR UPDATING TIMER');
+      console.log(result);
+      res.status(500).send('TIMER COULD NOT BE UPDATED');
     });
   }
   else {
@@ -140,14 +202,6 @@ router.put('/timers', function(req, res) {
     res.status(400).send({ error: 'Invalid Timer Object Requested' });
   }
 });
-
-function validateTimer(timer) {
-  return true;
-}
-
-function validateUser(user) {
-  return true;
-}
 
 /* POST register/login user */
 router.post('/users', function(req, res) {
@@ -188,5 +242,55 @@ router.post('/users', function(req, res) {
     res.status(400).send({ error: 'Invalid Timer Object', 'data':username });    
   }
 });
+
+/* GET all users */
+router.get('/users', function(req, res) {
+  app.dbo.collection('users').find().toArray()
+  .then(function(results) {
+    res.status(200).send(results);
+  }, function(error) {
+    console.log("ERROR");
+    console.log(error);
+    res.status(500).send({error: error});
+  });
+});
+
+/* GET specific user by uuid */
+router.get('/users/:oid', function(req, res) {
+  try {
+    var uuid = new ObjectId(req.params.oid);
+  }
+  catch (error) {
+    res.status(400).send({message: 'Invalid User ID Format', error: error.toString()});
+  }
+  
+  app.dbo.collection('users').findOne({_id: uuid})
+  .then(function(results) {
+    if (results == null) {
+      res.status(404).send({message: 'User not found'});
+    }
+    else {
+      res.status(200).send(results);
+    }
+  }, function(error) {
+    res.status(500).send({error: error});
+  });
+});
+
+// internal functions
+function validateTimer(timer) {
+  var isValid = false;
+  if (timer.name) {
+    if (timer.type === 'Countdown' || timer.type === 'Elapsed') {
+      isValid = true;
+    }
+  }
+
+  return isValid;
+}
+
+function validateUser(user) {
+  return true;
+}
 
 module.exports = router;
